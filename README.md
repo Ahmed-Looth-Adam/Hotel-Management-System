edb12ef6a7db3608a5f691dc967165a432395c26
Lunacy-simplified <231185110+Ahmed-Looth-Adam@users.noreply.github.com>
2025-10-10 16:34:47 +0500

    Made Authentication

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Hotel Management System
+
+# Superuser credentials
+## Name: admin
+## Email: admin@gmail.com
+## Pass: admin@123
---
+++ authentication/views.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html', {'user': request.user})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created successfully!")
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
