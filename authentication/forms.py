c83b86922bb2efdd9a511d65d8becc816f514ae6
Ahmed-Looth-Adam <s2400873@students.villacollege.edu.mv>
2025-10-19 12:25:05 +0500

    Email field added to registration

---
+++ authentication/forms.py
@@ -0,0 +1,20 @@
+# Edited By
+# -> Ahmed Looth Adam, UWE ID: 24050761
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,10 @@
 # Edited By
 # -> Ahmed Looth Adam, UWE ID: 24050761
 
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from .forms import CustomUserCreationForm
 
 @login_required
 def dashboard(request):
@@ -13,11 +13,11 @@ def dashboard(request):
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, "Account created successfully!")
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
